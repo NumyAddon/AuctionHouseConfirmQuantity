c7de38477bc85ee1e74402555e7d89008737f73a
Mark W <numedain@gmail.com>
2025-10-22 16:41:58 +0200

    Fixed the confirmation popup looking a bit janky

--- AuctionHouseConfirmQuantity.lua
+++ AuctionHouseConfirmQuantity.lua
@@ -1,14 +1,16 @@
-local _, private = ...
+EventUtil.ContinueOnAddOnLoaded('Blizzard_AuctionHouseUI', function()
+    local function buyNow(itemID, quantity)
+        C_AuctionHouse.ConfirmCommoditiesPurchase(itemID, quantity)
+    end
 
-local GetItemInfo = (GetItemInfo or C_Item.GetItemInfo)
+    local originalStartCommoditiesPurchase = AuctionHouseFrame.StartCommoditiesPurchase
+    AuctionHouseFrame.StartCommoditiesPurchase = function(self, itemID, quantity, unitPrice, totalPrice)
+        if quantity == 1 then
+            originalStartCommoditiesPurchase(self, itemID, quantity, unitPrice, totalPrice)
+            return
+        end
 
-private.BuyNow = function(itemID, quantity)
-    C_AuctionHouse.ConfirmCommoditiesPurchase(itemID, quantity)
-end
-
-private.CustomStartCommoditiesPurchase = function(self, itemID, quantity, unitPrice, totalPrice)
-    if (quantity > 1) then
-        local _, link, _ = GetItemInfo(itemID)
+        local _, link, _ = C_Item.GetItemInfo(itemID)
         local data = {
             itemID = itemID,
             count = quantity,
@@ -18,73 +20,60 @@ private.CustomStartCommoditiesPurchase = function(self, itemID, quantity, unitPr
             useLinkForItemInfo = true,
         };
         StaticPopup_Show('AUCTION_HOUSE_CONFIRM_PURCHASE_AMOUNT', quantity, link, data)
-
-        return
     end
 
-    self.BuyDialog:SetItemID(itemID, quantity, unitPrice, totalPrice);
-    self.BuyDialog:Show();
-    C_AuctionHouse.StartCommoditiesPurchase(itemID, quantity, unitPrice);
-end
+    StaticPopupDialogs['AUCTION_HOUSE_CONFIRM_PURCHASE_AMOUNT'] = {
+        text = 'You selected to buy %d %s, please confirm this.',
+        button1 = 'Confirm',
+        button2 = 'Cancel',
+        --- @param popup StaticPopupTemplate
+        OnShow = function(popup, data)
+            --- @type StaticPopupButtonTemplate
+            local button1 = popup.GetButton1 and popup:GetButton1() or popup.button1
+            --- @type StaticPopupTemplate_ItemFrame
+            local itemFrame = popup.GetItemFrame and popup:GetItemFrame() or popup.itemFrame
+            --- @type StaticPopupTemplate_MoneyFrame
+            local moneyFrame = popup.MoneyFrame or popup.moneyFrame
 
-if (C_AddOns.IsAddOnLoaded('Blizzard_AuctionHouseUI')) then
-    AuctionHouseFrame.StartCommoditiesPurchase = private.CustomStartCommoditiesPurchase
-else
-    private.frame = CreateFrame('Frame')
-    private.frame:HookScript('OnEvent', function(self, event, addon, ...)
-        if addon == 'Blizzard_AuctionHouseUI' then
-            AuctionHouseFrame.StartCommoditiesPurchase = private.CustomStartCommoditiesPurchase
-        end
-    end)
-    private.frame:RegisterEvent('ADDON_LOADED')
-end
-
-StaticPopupDialogs['AUCTION_HOUSE_CONFIRM_PURCHASE_AMOUNT'] = {
-    text = 'You selected to buy %s %s, please confirm this.',
-    button1 = 'Confirm',
-    button2 = 'Cancel',
-    --- @param popup StaticPopupTemplate
-    OnShow = function(popup, data)
-        local button1 = popup.GetButtons and popup:GetButtons()[1] or popup.button1
-        local itemFrame = popup.GetItemFrame and popup:GetItemFrame() or popup.itemFrame
-        local moneyFrame = popup.MoneyFrame or popup.moneyFrame
-
-        button1:Disable()
-        MoneyFrame_Update(moneyFrame, data.totalPrice)
-        C_AuctionHouse.StartCommoditiesPurchase(data.itemID, data.count, data.unitPrice);
-
-        itemFrame:ClearAllPoints();
-        itemFrame:SetPoint('BOTTOM', popup, 'BOTTOM', -60, 80)
-    end,
-    OnAccept = function(popup, data)
-        private.BuyNow(data.itemID, data.count)
-    end,
-    EditBoxOnEscapePressed = function(editBox)
-        editBox:GetParent():Hide()
-    end,
-    EditBoxOnEnterPressed = function(editBox)
-        --- @type StaticPopupTemplate
-        local popup = editBox:GetParent()
-        local button1 = popup.GetButtons and popup:GetButtons()[1] or popup.button1
-        if button1:IsEnabled() then
-            StaticPopup_OnClick(popup, 1)
-        end
-    end,
-    EditBoxOnTextChanged = function(editBox, data)
-        --- @type StaticPopupTemplate
-        local popup = editBox:GetParent()
-        local button1 = popup.GetButtons and popup:GetButtons()[1] or popup.button1
-        if (editBox:GetText() == data.count .. '') then
-            button1:Enable()
-        else
             button1:Disable()
-        end
-    end,
-    hasItemFrame = true,
-    hasMoneyFrame = true,
-    hasEditBox = true,
-    timeout = 0,
-    whileDead = true,
-    hideOnEscape = true,
-    preferredIndex = 3,
-}
+            MoneyFrame_Update(moneyFrame, data.totalPrice)
+            C_AuctionHouse.StartCommoditiesPurchase(data.itemID, data.count, data.unitPrice);
+
+            itemFrame:ClearAllPoints();
+            itemFrame:SetPoint('BOTTOM', popup, 'BOTTOM', -60, 80)
+        end,
+        OnAccept = function(popup, data)
+            buyNow(data.itemID, data.count)
+        end,
+        EditBoxOnEscapePressed = function(editBox)
+            editBox:GetParent():Hide()
+        end,
+        EditBoxOnEnterPressed = function(editBox)
+            --- @type StaticPopupTemplate
+            local popup = editBox:GetParent()
+            --- @type StaticPopupButtonTemplate
+            local button1 = popup.GetButton1 and popup:GetButton1() or popup.button1
+            if button1:IsEnabled() then
+                StaticPopup_OnClick(popup, 1)
+            end
+        end,
+        EditBoxOnTextChanged = function(editBox, data)
+            --- @type StaticPopupTemplate
+            local popup = editBox:GetParent()
+            --- @type StaticPopupButtonTemplate
+            local button1 = popup.GetButton1 and popup:GetButton1() or popup.button1
+            if (editBox:GetText() == data.count .. '') then
+                button1:Enable()
+            else
+                button1:Disable()
+            end
+        end,
+        hasItemFrame = true,
+        hasMoneyFrame = true,
+        hasEditBox = true,
+        timeout = 0,
+        whileDead = true,
+        hideOnEscape = true,
+        preferredIndex = 3,
+    }
+end)
